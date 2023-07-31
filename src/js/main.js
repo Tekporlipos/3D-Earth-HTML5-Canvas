f9eee6df7c76d3cc293827722a51f3ff137790ed
John Dzikunu <56548679+tekpor@users.noreply.github.com>
2023-07-31 10:40:18 +0000

    update:code refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,120 +1,120 @@
-var camera, scene, renderer;
-		var earth, cloud;
-		var pointLight, ambientLight;
-		var mouseDown = false, mouseX = 0, mouseY = 0;
+let camera, scene, renderer;
+let earthMesh, cloudMesh;
+let pointLight, ambientLight;
+let isMouseDown = false, prevMouseX = 0, prevMouseY = 0;
 
-		init();
-		animate();
+init();
+animate();
 
-		function init() {
-			// initialization
-			scene = new THREE.Scene();
-			camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-			camera.position.z = 160;
+function init() {
+  // Initialization
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.z = 160;
 
-			// Earth terrain
-			var earth_texture  = new THREE.TextureLoader().load("src/images/earth.jpeg");
-			var earth_bump     = new THREE.TextureLoader().load("src/images/bump.jpeg");
-			var earth_specular = new THREE.TextureLoader().load("src/images/spec.jpeg");
-			var earth_geometry = new THREE.SphereGeometry(30, 32, 32);
-			var earth_material = new THREE.MeshPhongMaterial({
-				shininess   : 40,
-				bumpScale   : 1,
-				map         : earth_texture,
-				bumpMap     : earth_bump,
-				specularMap : earth_specular
-			});
-			earth = new THREE.Mesh(earth_geometry, earth_material);
-			scene.add(earth);
+  // Earth terrain
+  let earthTexture = new THREE.TextureLoader().load("src/images/earth.jpeg");
+  let earthBump = new THREE.TextureLoader().load("src/images/bump.jpeg");
+  let earthSpecular = new THREE.TextureLoader().load("src/images/spec.jpeg");
+  let earthGeometry = new THREE.SphereGeometry(30, 32, 32);
+  let earthMaterial = new THREE.MeshPhongMaterial({
+    shininess: 40,
+    bumpScale: 1,
+    map: earthTexture,
+    bumpMap: earthBump,
+    specularMap: earthSpecular
+  });
+  earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
+  scene.add(earthMesh);
 
-			// Earth cloud
-			var cloud_texture  = new THREE.TextureLoader().load( 'src/images/cloud.png');
-			var cloud_geometry = new THREE.SphereGeometry(31, 32, 32);
-			var cloud_material = new THREE.MeshBasicMaterial({
-				shininess   : 10,
-				map         : cloud_texture,
-				transparent : true,
-				opacity     : 0.8
-			});
-			cloud = new THREE.Mesh(cloud_geometry, cloud_material);
-			scene.add(cloud);
+  // Earth cloud
+  let cloudTexture = new THREE.TextureLoader().load('src/images/cloud.png');
+  let cloudGeometry = new THREE.SphereGeometry(31, 32, 32);
+  let cloudMaterial = new THREE.MeshBasicMaterial({
+    shininess: 10,
+    map: cloudTexture,
+    transparent: true,
+    opacity: 0.8
+  });
+  cloudMesh = new THREE.Mesh(cloudGeometry, cloudMaterial);
+  scene.add(cloudMesh);
 
-			// point light (upper left)
-			pointLight = new THREE.PointLight(0xffffff);
-			pointLight.position.set(-400, 100, 150);
-			scene.add (pointLight);
+  // Point light (upper left)
+  pointLight = new THREE.PointLight(0xffffff);
+  pointLight.position.set(-400, 100, 150);
+  scene.add(pointLight);
 
-			// ambient light
-			ambientLight = new THREE.AmbientLight(0x222222);
-			scene.add(ambientLight);
+  // Ambient light
+  ambientLight = new THREE.AmbientLight(0x222222);
+  scene.add(ambientLight);
 
-			// renderer
-			renderer = new THREE.WebGLRenderer({alpha: true});
-			renderer.setClearColor(0xffffff, 0);
-			renderer.setPixelRatio(window.devicePixelRatio);
-			renderer.setSize(window.innerWidth, window.innerHeight);
-			document.body.appendChild(renderer.domElement);
+  // Renderer
+  renderer = new THREE.WebGLRenderer({ alpha: true });
+  renderer.setClearColor(0xffffff, 0);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-			// event handler
-			window.addEventListener('resize', onWindowResize, false);
-			document.addEventListener('mousemove', function (e) {onMouseMove(e);}, false);
-			document.addEventListener('mousedown', function (e) {onMouseDown(e);}, false);
-			document.addEventListener('mouseup', function (e) {onMouseUp(e);}, false);
-			document.addEventListener('wheel', function (e) {onMouseWheel(e);}, false);
-		}
+  // Event handlers
+  window.addEventListener('resize', onWindowResize, false);
+  document.addEventListener('mousemove', onMouseMove, false);
+  document.addEventListener('mousedown', onMouseDown, false);
+  document.addEventListener('mouseup', onMouseUp, false);
+  document.addEventListener('wheel', onMouseWheel, false);
+}
 
-		function animate() {
-			requestAnimationFrame(animate);
-			earth.rotation.y += 0.001;
-			cloud.rotation.y += 0.001;
-			renderer.render(scene, camera);
-		}
+function animate() {
+  requestAnimationFrame(animate);
+  earthMesh.rotation.y += 0.001;
+  cloudMesh.rotation.y += 0.001;
+  renderer.render(scene, camera);
+}
 
-		function onWindowResize() {
-			camera.aspect = window.innerWidth / window.innerHeight;
-			camera.updateProjectionMatrix();
-			renderer.setSize(window.innerWidth, window.innerHeight);
-		}
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-		function onMouseMove(evt) {
-			if (!mouseDown) return;
-			evt.preventDefault();
-			var deltaX = evt.clientX - mouseX, deltaY = evt.clientY - mouseY;
-			mouseX = evt.clientX;
-			mouseY = evt.clientY;
-			rotateScene(deltaX, deltaY);
-		}
+function onMouseMove(evt) {
+  if (!isMouseDown) return;
+  evt.preventDefault();
+  let deltaX = evt.clientX - prevMouseX, deltaY = evt.clientY - prevMouseY;
+  prevMouseX = evt.clientX;
+  prevMouseY = evt.clientY;
+  rotateScene(deltaX, deltaY);
+}
 
-		function onMouseDown(evt) {
-			evt.preventDefault();
-			mouseDown = true;
-			mouseX = evt.clientX;
-			mouseY = evt.clientY;
-		}
+function onMouseDown(evt) {
+  evt.preventDefault();
+  isMouseDown = true;
+  prevMouseX = evt.clientX;
+  prevMouseY = evt.clientY;
+}
 
-		function onMouseUp(evt) {
-			evt.preventDefault();
-			mouseDown = false;
-		}
+function onMouseUp(evt) {
+  evt.preventDefault();
+  isMouseDown = false;
+}
 
-		function rotateScene(deltaX, deltaY) {
-		  earth.rotation.y += deltaX / 300;
-		  earth.rotation.x += deltaY / 300;
-		  cloud.rotation.y += deltaX / 300;
-		  cloud.rotation.x += deltaY / 300;
-		}
+function rotateScene(deltaX, deltaY) {
+  earthMesh.rotation.y += deltaX / 300;
+  earthMesh.rotation.x += deltaY / 300;
+  cloudMesh.rotation.y += deltaX / 300;
+  cloudMesh.rotation.x += deltaY / 300;
+}
 
-		function onMouseWheel(evt) {
-			evt.preventDefault();
-			let zoomFactor = 1.1;
-			let delta = evt.deltaY || evt.wheelDelta;
-			let zoomAmount = (delta > 0) ? 1 / zoomFactor : zoomFactor;
-		
-			// Adjust the camera's position based on zoom amount.
-			camera.position.z *= zoomAmount;
-		
-			// Limit the zoom range to prevent going too far or too close.
-			let maxZoom = 1000;
-			let minZoom = 50; 
-			camera.position.z = Math.min(Math.max(camera.position.z, minZoom), maxZoom);
-		}
+function onMouseWheel(evt) {
+  evt.preventDefault();
+  let zoomFactor = 1.1;
+  let delta = evt.deltaY || evt.wheelDelta;
+  let zoomAmount = (delta > 0) ? 1 / zoomFactor : zoomFactor;
+
+  // Adjust the camera's position based on zoom amount.
+  camera.position.z *= zoomAmount;
+
+  // Limit the zoom range to prevent going too far or too close.
+  let maxZoom = 1000;
+  let minZoom = 50;
+  camera.position.z = Math.min(Math.max(camera.position.z, minZoom), maxZoom);
+}
